f63aa840a479370225c8288b54bed7d84906ca11
alexzoo0 <alexisvelasquez15@gmail.com>
2022-01-25 11:23:11 -0600

    test jest fucntionality set

---
+++ tests/Engineer-test.js
@@ -0,0 +1,19 @@
+const Engineer = require('../lib/Engineer');
+
+test('engineer object', () => {
+    const engineer = new Engineer('Alexis', 7654, 'alexisvelasquez15@gmail.com', 'Alexzoo0');
+
+    expect(engineer.github()).toEqual(expect.any(String));
+});
+
+test('engineer github', () => {
+    const engineer = new Engineer('Alexis', 7654, 'alexisvelasquez15@gmail.com', 'Alexzoo0');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('Employee Role', () => {
+    const engineer = new Engineer('Alexis', 7654, 'alexisvelasquez15@gmail.com', 'Alexzoo0');
+
+    expect(engineer.getRole()).toEqual('Engineer');
+});
---
+++ tests/Intern-test.js
@@ -0,0 +1,19 @@
+const Intern = require('../lib/Intern');
+
+test('Intern Object', () => {
+    const intern = new Intern('Alexis', 7654, 'alexisvelasquez15@gmail.com', 'UT');
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+test('Intern School', () => {
+    const intern = new Intern('Alexis', 7654, 'alexisvelasquez15@gmail.com', 'UT');
+
+    expect(intern.school).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+test('Employee Role', () => {
+    const intern = new Intern('Alexis', 7654, 'alexisvelasquez15@gmail.com', 'UT');
+
+    expect(intern.getRole).toEqual('Intern');
+});
